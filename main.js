c84e5c62fa2c7b7023d582661ab2503f12ede77e
niederle <claude.niederlender@inist.fr>
2017-01-06 17:21:57 +0100

    Passage de sync-request à request + passage de exec à spawnSync

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-const syncRequest = require('sync-request');
 const fs = require('fs');
-const exec = require('child_process').exec;
+const spawnSync = require('child_process').spawnSync;
 const JSSelect = require('js-select');
+const requestModule = require('request');
+const proxyOpts = (process.env['http_proxy']) ? {proxy:process.env['http_proxy'],timeout:10000} : {timeout:10000};
+const request = requestModule.defaults(proxyOpts);
+const each = require('async/each');
+const path = require('path');
+
+const kbartRelativePath = "resources/kbart/";
 
 // the url where the Google Scholar xml files will put exposed on the internet
 const url = 'https://content-delivery.istex.fr/google-scholar';
@@ -24,85 +30,86 @@ var packageGet = {
     method : 'GET' 
 };
 
-function generateGoogleScholarFiles(gsFilesPath, kbartPath, outPath) {
-	console.log("Generating Google Scholar description files");
-	
-    var istexPackages = [];
-	// first get the list of BACON package names via bacon.abes.fr/list.json
+function generateGoogleScholarFiles(gsFilesPath, kbartPath, outDir) {
+  console.log("Generating Google Scholar description files");
+  
+  var istexPackages = [];
+  // first get the list of BACON package names via bacon.abes.fr/list.json
+  
+  console.log("Getting ISTEX packages via BACON service");
+  // we use the BACON service filter parameters for getting only the ISTEX package names
+  const baconUrl = 'http://bacon.abes.fr/filter/providerid=0&standardpackage=0&masterlist=0&labelled=0&istex=1&mixte=1&monograph=1&serial=1';
+  request.get(baconUrl, function (error, response, body) {
 
-	console.log("Getting ISTEX packages via BACON service");
-	// we use the BACON service filter parameters for getting only the ISTEX package names
-	var response = syncRequest('GET', 
-		'http://bacon.abes.fr/filter/providerid=0&standardpackage=0&masterlist=0&labelled=0&istex=1&mixte=1&monograph=1&serial=1', 
-		{timeout : 30000});
-	if (response && (response.statusCode == 200)) {
-        // get all the package names containing the string 'ISTEX'
-        var responseText = response.body.toString();
-		var json = JSON.parse(responseText);
-		var nodes = JSSelect(json, ".bacon .package_id").nodes();
-		for(var node in nodes) {
-			addISTEXPackage(nodes[node], istexPackages);
-		}
-	} else {
-    	console.error('Error code ' + response.statusCode);
+    if (response && (response.statusCode == 200)) {
+      // get all the package names containing the string 'ISTEX'
+      var responseText = response.body.toString();
+      var json = JSON.parse(responseText);
+      var nodes = JSSelect(json, ".bacon .package_id").nodes();
+      for(var node in nodes) {
+        addISTEXPackage(nodes[node], istexPackages);
+      }
+    } else {
+      console.error('Error code ' + response.statusCode);
+      process.exit(1);
     }
-
+  
     // get the Kbart files for the ISTEX packages via bacon.abes.fr/package2kbart/
-    console.log(istexPackages);
-	
+    console.log("istex packages : ",istexPackages);
+    
     // update the local kbart files based on the list of istex names
-    for (var i = 0, len = istexPackages.length; i < len; i++) {
-    	updateISTEXKbartPackage(istexPackages[i], "resources/kbart/");
-    };
+    each(istexPackages, updateISTEXKbartPackage, function(err) {
+      if (err) {
+        console.log("erreur lors de la MAJ des packages ABES:",err);
+        return(err);
+      }
 
-	// list of generated Google files
-    var holdingFiles = [];
-
-	// for each Kbart file in xml to be generated into Google XML format, apply the gs style sheet 
-	for (var i = 0, len = toBeGenerated.length; i < len; i++) {	
-		// collection name must match with the KBART name
-		var collection = toBeGenerated[i];
-		var filename = kbartPath + collection + '.xml';
-	    var command = xsltEngine + ' ' + pathKbart2gs + ' ' + filename;
-	    console.log('transforming with external command: ' + command);
-	    exec(command, {maxBuffer: 1024 * 1000}, function (error, stdout, stderr) {
-	       if (stderr) {
-	           console.error(stderr);
-	       }
-	       if (error) {
-	           console.error(error);
-	       }
-	       fs.writeFile("results/institutional_holdings_"+collection+".xml", stdout, function(err) {
-	            if(err) {
-	                return console.log(err);
-	            }
-	            console.log("File institutional_holdings_"+collection+".xml saved under results/");
-	        });
-	    });
-
-		// note: a holding file cannot have more than 1 MB of data!
-		holdingFiles.push("institutional_holdings_"+collection+".xml");
-	};
-	
-	// write ISTEX institutional_links file which will refer to the previous holding file(s)
-	fs.readFile("resources/institutional_links_istex.xml", "utf8", function (err,data) {
-	  	if (err) {
-	    	return console.log(err);
-	  	}
-		var replacementString = '<electronic_holdings>\n';
-		for(var holdingFile in holdingFiles) {
-			replacementString += '\t\t<url>' + url + '/' + holdingFiles[holdingFile] + '</url>\n';
-		}
-		replacementString += '\t</electronic_holdings>';
-	  	data = data.replace("<electronic_holdings/>", replacementString);
-		fs.writeFile("results/institutional_links_istex.xml", data, function(err) {
-		    if(err) {
-		        return console.log(err);
-		    }
-		    console.log("File institutional_links_istex.xml saved under results/");
-		});
-	});
-
+      // list of generated Google files
+      var holdingFiles = [];
+  
+      // for each Kbart file in xml to be generated into Google XML format, apply the gs style sheet
+      for (var i = 0, len = toBeGenerated.length; i < len; i++) {
+        // collection name must match with the KBART name
+        var collection = toBeGenerated[i];
+        var inputPath = kbartPath + collection + '.xml';
+        var outHoldingPath = path.join(outDir, "institutional_holdings_" + collection + ".xml");
+        var xsltArgs = ['-o', outHoldingPath, pathKbart2gs, inputPath];
+        console.log('transforming with external command xsltproc + arguments : ' + xsltArgs);
+        
+        try {
+          var spawnResult = spawnSync(xsltEngine, xsltArgs);
+          console.log("File institutional_holdings_"+collection+".xml saved under results/");
+        } catch (xsltException) {
+          console.error(xsltException);
+          return(xsltException);
+        }
+        
+        // // note: a holding file cannot have more than 1 MB of data!
+        holdingFiles.push("institutional_holdings_"+collection+".xml");
+      }
+  
+      // write ISTEX institutional_links file which will refer to the previous holding file(s)
+      fs.readFile("resources/institutional_links_istex.xml", "utf8", function (err,data) {
+        if (err) {
+          return console.log(err);
+        }
+        var replacementString = '<electronic_holdings>\n';
+        for(var holdingFile in holdingFiles) {
+          replacementString += '\t\t<url>' + url + '/' + holdingFiles[holdingFile] + '</url>\n';
+        }
+        replacementString += '\t</electronic_holdings>';
+        data = data.replace("<electronic_holdings/>", replacementString);
+        fs.writeFile("results/institutional_links_istex.xml", data, function(err) {
+          if(err) {
+            return console.log(err);
+          }
+          console.log("File institutional_links_istex.xml saved under results/");
+        });
+      });
+      
+    });
+    
+  });
 }
 
 function addISTEXPackage(package_id, istexPackages) {
@@ -116,20 +123,23 @@ function addISTEXPackage(package_id, istexPackages) {
 }
 
 
-function updateISTEXKbartPackage(package_id, path) {
-	var response = syncRequest("GET", 
-		"https://bacon.abes.fr/package2kbart/"+package_id+".xml", 
-		{timeout : 30000});
-	if (response && (response.statusCode == 200)) {
-		var responseText = response.body.toString();		
-		// save locally the delivered XML Kbart file
-		try {
-			fs.writeFileSync(path+package_id+".xml", responseText);
-			console.log("Package " + package_id +" successfully updated.");
-		} catch(err) {
-			console.log("Opps: " + err);
-		}
-	}
+function updateISTEXKbartPackage(package_id, updateCallback) {
+  var packageUrl = "https://bacon.abes.fr/package2kbart/"+package_id+".xml"
+  console.log("packageUrl = ",packageUrl)
+  var response = request.get(packageUrl, function(error, response, body) {
+    if (response && (response.statusCode == 200)) {
+      var responseText = response.body.toString();
+      // save locally the delivered XML Kbart file
+      try {
+        fs.writeFileSync(kbartRelativePath + package_id + ".xml", responseText);
+        console.log("Package " + package_id + " successfully updated.");
+        return updateCallback();
+      } catch (err) {
+        console.log("Opps: " + err);
+        return updateCallback(err)
+      }
+    } else return updateCallback(error);
+  });
 }
 
 
