a4d820f8b109592bfeb4255d04d207a021aebfbd
Patrice Lopez <patrice.lopez@science-miner.com>
2017-02-27 12:47:42 +0100

    Improve test feedback

--- test.js
+++ test.js
@@ -6,6 +6,13 @@ const baseOpenURL = 'https://api.istex.fr/document/openurl';
 
 const testPaths = ['resources/test/google-scholar-openurls.txt', 'resources/test/openurls-0.1.txt'];
 
+const green = '\x1b[32m';
+const red = '\x1b[31m';
+const orange = '\x1b[33m';
+const white = '\x1b[37m';
+const score = '\x1b[4m';
+const reset = '\x1b[0m';
+
 function testOpenURLSet(testFilePaths) {
     var correct = 0;
 	var total = 0;
@@ -20,8 +27,8 @@ function testOpenURLSet(testFilePaths) {
                     total++;
                     var values = line.split('\t');
                     if ((values.length != 3) && (values.length != 2)) {
-                        console.error('Malformed test line: ' + line);
-                        console.error('Invalid number of tokens: ' + values.length + " tokens");
+                        console.error(red, 'Malformed test line: ' + line);
+                        console.error(red, 'Invalid number of tokens: ' + values.length + " tokens");
                     } else {
                         var openURL = values[0];
                         var expectedResult = values[1];
@@ -29,13 +36,13 @@ function testOpenURLSet(testFilePaths) {
                         if (values.length == 3)
                             resourceURL = values[2];
                         else if (expectedResult == 1) {
-                            console.error('Malformed test line: ' + line);
-                            console.error('Missing expected result as third token');
+                            console.error(red, 'Malformed test line: ' + line);
+                            console.error(red, 'Missing expected result as third token');
                             return;
                         }
 
                         var urlCall = baseOpenURL+openURL+"&noredirect";
-                        console.log('calling: ' + urlCall);
+                        console.log(white, '\n' + urlCall);
                         try {
                         	var response = request('GET', urlCall, {timeout : 5000});
 	                        if (response && (response.statusCode == 200)) {
@@ -51,18 +58,23 @@ function testOpenURLSet(testFilePaths) {
                                         resUrl = resUrl.replace("?sid=google","");
 		                            	if (resUrl === resourceURL) {
 			                            	correct++;
+                                            console.log(green, 'True positive / Resource found as expected');
                                         } else {
-                                            console.log('Resource does not match expected one');
+                                            console.log(red, 'False positive / Resource found but does not match expected one');
                                         }
 		                        	}
-		                        }
+		                        } else {
+                                    console.log(red, 'False positive / A resource is incorreclty found');
+                                }
 	                        }
 	                        else if (response && (response.statusCode == 404)) {
-	                        	if (expectedResult == 0)
+	                        	if (expectedResult == 0) {
 		                            correct++;
-	                        	console.log('Resource not found');
+                                    console.log(green, 'True negative / Resource not found as expected');
+                                } else
+    	                        	console.log(red, 'False negative / Resource incorrectly not found');
 	                        } else {
-	                        	console.log('Error code ' + response.statusCode);
+	                        	console.log(orange, 'Error code ' + response.statusCode);
 	                        }
                         } catch(err) {
                         	console.error(err);
@@ -70,7 +82,9 @@ function testOpenURLSet(testFilePaths) {
                     }
                 }
             });
-			console.log("correct/total: " + correct+"/"+total + ", " + (100 * correct/total) + "%");   
+            console.log(reset, "");
+			console.log(score, "correct/total: " + correct+"/"+total + ", " + (100 * correct/total) + "%");  
+            console.log(reset, "\n");
         });
     }
 }
